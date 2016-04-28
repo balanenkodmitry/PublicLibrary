b33ecb06dc83673346d85c5e787ba72d0e786e59
dbal <dbal@PC-DBAL>
2016-04-28 11:24:57 +0300

    Paging, Sorting, Find

---
+++ PublicLibrary.Web/Models/Books/BooksViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PublicLibrary.Domain;
+
+namespace PublicLibrary.Web.Models.Books
+{
+    public class BooksViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
